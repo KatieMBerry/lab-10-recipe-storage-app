df7b4278f4a4820fe32415bd5877dfc20a6e0a0d
Katie <katieberrym@gmail.com>
2020-12-15 15:07:50 -0800

    added log class and controller file with routes

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,17 @@
-DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS recipes CASCADE;
+DROP TABLE IF EXISTS logs;
 
 CREATE TABLE recipes (
   id BIGINT GENERATED ALWAYS AS IDENTITY,
   name TEXT NOT NULL,
   directions TEXT[]
+);
+
+CREATE TABLE logs (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  name TEXT NOT NULL,
+  recipe_id INTEGER NOT NULL,
+  date_of_event TEXT,
+  notes TEXT,
+  rating INTEGER CHECK (rating > 0)
 );
